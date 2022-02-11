3a5946c5d66268d4dbc5fcc619d2aa04346ff2b6
Esha Naz <eshanaz76@gmail.com>
2022-02-11 05:29:35 +0500

    ork complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React , {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [isCool, setIsDarkMode] = useState(true);
+  const display = document.querySelector('body');
+  display.className = isCool ? "Light" : "Dark";
+  
+  const mode = () => {
+    setIsDarkMode(!isCool);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div>
+        <div class="fontStyle"> 
+          <label class="boX">
+            <input type="checkbox"></input>
+            <span onClick={mode} className="slider round"> </span>
+          </label>
+          <br></br>
+          <h1 >SWIPE FOR DARK MODE AS WELL AS LIGHT MODE :)</h1>
+          <br />
+          <h1>BYE BYE</h1>
+          </div>
+         
+        </div>
+        
   );
-}
+  }
 
 export default App;
